17fc67855330b2dcaeb0b699d9630198ca349ab2
yemo0 <1735681885@qq.com>
2022-01-21 01:42:06 +0800

    delete sharePage

--- lib/common/routers.dart
+++ lib/common/routers.dart
@@ -1,13 +1,11 @@
 import 'package:wifi/pages/home.dart';
 import 'package:wifi/pages/loding.dart';
-import 'package:wifi/pages/share.dart';
 import 'package:wifi/pages/wifi_no_root.dart';
 import 'package:wifi/pages/wifi_root.dart';
 
 final routers = {
   'home': (context) => Home(title: "text"),
   'loading': (context) => Loading(),
-  'share': (context) => Share(),
   'wifi_no_root': (context) => WifiNoRoot(),
   'wifi_root': (context) => WiFiRoot(),
 };
